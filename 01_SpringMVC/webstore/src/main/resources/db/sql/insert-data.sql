fb8e573ca7fe753133f3fdfcc2901f45327ad60f
Johnny Xu <gaga1227@gmail.com>
2017-01-11 13:52:52 +1100

    chap2 - completed

--- 01_SpringMVC/webstore/src/main/resources/db/sql/insert-data.sql
+++ 01_SpringMVC/webstore/src/main/resources/db/sql/insert-data.sql
@@ -1,3 +1,4 @@
+/*products*/
 INSERT INTO PRODUCTS VALUES ('P1234', 'iPhone 6s',
                       'Apple iPhone 6s smartphone with 4.00-inch 640x1136 display and 8-megapixel rear camera','500','Apple','Smartphone','New',450,0,false);
 
@@ -6,5 +7,9 @@ INSERT INTO PRODUCTS VALUES ('P1235', 'Dell Inspiron',
                      700,'Dell','Laptop','New',1000,0,false);
 
 INSERT INTO PRODUCTS VALUES ('P1236', 'Nexus 7',
-                     'Google Nexus 7 is the lightest 7 inch tablet With a quad-core Qualcomm Snapdragon™ S4 Pro processor',
+                     'Google Nexus 7 is the lightest 7 inch tablet With a quad-core Qualcomm Snapdragon S4 Pro processor',
                       300,'Google','Tablet','New',1000,0,false);
+
+/*customers*/
+INSERT INTO CUSTOMERS VALUES ('C001', 'John Smith', 'Unit 1, 2 some address, some suburb, VIC 3000', 1);
+INSERT INTO CUSTOMERS VALUES ('C002', 'Jane Smith', 'Unit 2, 2 some address, some suburb, VIC 3000', 12);
