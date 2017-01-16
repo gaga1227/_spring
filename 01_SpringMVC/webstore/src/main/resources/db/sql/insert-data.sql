483db7279238e65406ca9426cdd9752c6d399463
Johnny Xu <gaga1227@gmail.com>
2017-01-16 15:17:45 +1100

    chap10 - revert previous customer related codes

--- 01_SpringMVC/webstore/src/main/resources/db/sql/insert-data.sql
+++ 01_SpringMVC/webstore/src/main/resources/db/sql/insert-data.sql
@@ -10,6 +10,3 @@ INSERT INTO PRODUCTS VALUES ('P1236', 'Nexus 7',
                      'Google Nexus 7 is the lightest 7 inch tablet With a quad-core Qualcomm Snapdragon S4 Pro processor',
                       300,'Google','Tablet','New',1000,0,false);
 
-/*customers*/
-INSERT INTO CUSTOMERS VALUES ('C001', 'John Smith', 'Unit 1, 2 some address, some suburb, VIC 3000', 1);
-INSERT INTO CUSTOMERS VALUES ('C002', 'Jane Smith', 'Unit 2, 2 some address, some suburb, VIC 3000', 12);
